33fa6e03c78847585b49c6a156a0b69b8a48f8ae
Bucsy Benjamin <bucsy.benjamin@simonyi.bme.hu>
2024-04-21 13:13:49 +0200

    add seed to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+TEMPLATE_IMAGE_PATH = Rails.root.join('db', 'images', 'placeholder.jpg')
+
+puts "purging db"
+Photo.delete_all
+
+20.times do |i|
+
+  puts "Creating fake image ##{i}"
+  # Generate a fake name using Faker
+  name = Faker::Movie.title
+
+  # Create a new Photo instance with the generated name
+  time = Faker::Date.between(from: '2014-09-23', to: Date.today)
+
+  photo = Photo.new(name: name, created_at: time)
+
+  # Attach the template image
+  photo.image.attach(io: File.open(TEMPLATE_IMAGE_PATH), filename: 'template_image.jpg', content_type: 'image/jpeg')
+
+  # Save the Photo entry
+  photo.save
+
+  # This is needed because sqlite3 is so slow
+  sleep(0.5)
+end
